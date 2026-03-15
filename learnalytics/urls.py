864b6e738104f7908764ed35fcd0cd25c7ee2347
Joey-1123 <shubhampanchal9168@gmail.com>
2026-03-15 22:38:37 +0530

    Add password reset and change password feature with templates

--- learnalytics/urls.py
+++ learnalytics/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for learnalytics project.
+Defines URL patterns for the learnalytics Django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -34,4 +34,58 @@ urlpatterns = [
 
     # Logout
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+    # ---------------------------
+    # Password Reset
+    # ---------------------------
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='auth/password_reset.html'
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='auth/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='auth/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='auth/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+
+    # ---------------------------
+    # Change Password (while logged in)
+    # ---------------------------
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='auth/password_change.html'
+        ),
+        name='password_change'
+    ),
+
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='auth/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
 ]
